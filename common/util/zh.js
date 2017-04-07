e9eafa19f40bcef15de5a19cda0373fc12cf1c85
huihuawk <huihua_wk@163.com>
2017-04-07 15:40:04 +0800

    [IMP]修改GIT忽略规则，添加request输出阶段提示

--- common/util/zh.js
+++ common/util/zh.js
@@ -6,19 +6,18 @@ var CronJob = require('cron').CronJob;
 
 var dao = new ArticleDAO();
 
-var i = 0;
-
-new CronJob('* * *', function () {
-    console.log('i: '+ ++i);
-}, null, true, 'Asia/Shanghai')
 
 var x = 1;
 var historyDAO = new HistoryDAO();
 new CronJob('* * *',function () {
     if(x == 2){
-        dlAPI.getHistory('20170401').then(function (history) {
+        console.log('===========  begin request  ===========')
+        dlAPI.getHistory('20170301').then(function (history) {
             console.log(history);
+            console.log('===========  over request  ===========')
         })
     }
     console.log('x: '+ ++x);
-}, null, true, 'Asia/Shanghai')
+}, function () {
+    console.log('CronJob over')
+}, true, 'Asia/Shanghai')
